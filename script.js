e2cf1ecd87627f6e3870ba4cc0218213603d9b9d
guineverehooley-coder <guineverehooley@students.berkeley.net>
2026-03-26 00:30:45 +0000

    finished

--- script.js
+++ script.js
@@ -1,9 +1,165 @@
-//starter code
+// function sleep (name){
+//   console.log (name + " sleeps for 8 hours");
+// }
 
+
+// var favoriteFood= "bacon";
+
+// function eat(name,food){
+//   console.log (name + " eats " + food);
+//   if(food==favoriteFood){
+//     console.log ("YUM!!! " +  name + " wants more " +food);
+//   }else{
+//     sleep(name);
+//   }
+// }
+
+// class Tiger extends Animal {
+
+//   constructor(name) {
+//     this.name = name
+//     this.favoriteFood = "meat";
+//   }
+
+//   sleep() {
+//     console.log(this.name + " sleeps for 8 hours");
+//   }
+
+//   eat(food) {
+//     console.log(this.name + " eats " + food);
+//     if (food == this.favoriteFood) {
+//       console.log("YUM!!! " + this.name + " wants more " + food);
+//     } else {
+//       this.sleep(this.name);
+//     }
+//   }
+// }
+
+
+
+// class Bear extends Animal{
+//   constructor(name) {
+//     this.name = name
+//     this.favoriteFood = "fish";
+//   }
+
+//   sleep() {
+//     console.log(this.name + " hibernates for 4 months");
+//   }
+
+//   eat(food) {
+//     console.log(this.name + " eats " + food);
+//     if (food == this.favoriteFood) {
+//       console.log("YUM!!! " + this.name + " wants more " + food);
+//     } else {
+//       this.sleep(this.name);
+//     }
+//   }
+// }
+var animalPopulation=0;
+class Animal {
+
+  constructor(name, favoriteFood) {
+    this.name = name;
+    this.favoriteFood = favoriteFood;
+    animalPopulation++;
+  }
+  static getPopulation() {
+	return animalPopulation;
+}
+  sleep() {
+    console.log(this.name + " sleeps for 8 hours");
+  }
+
+  eat(food) {
+    console.log(this.name + " eats " + food);
+    if (food == this.favoriteFood) {
+      console.log("YUM!!! " + this.name + " wants more " + food);
+    } else {
+      this.sleep();
+    }
+  }
+}
+class Tiger extends Animal {
+  constructor(name) {
+    super(name, "meat");
+  }
+}
+
+class Bear extends Animal {
+  constructor(name) {
+    super(name, "fish");
+  }
+  sleep() {
+    console.log(this.name + " hibernates for 4 months");
+  }
+}
+class Unicorn extends Animal {
+  constructor(name) {
+    super(name, "marshmallows");
+  }
+  sleep() {
+    console.log(this.name + " sleeps in a cloud");
+  }
+}
+class Giraffe extends Animal {
+  constructor(name) {
+    super(name, "leaves");
+  }
+  eat(food) {
+    if (food == this.favoriteFood) {
+      super.eat(food);
+    } else {
+      console.log("YUCK!!! " + this.name + " will not eat " + food)
+    }
+  }
+}
+class Bee extends Animal {
+  constructor(name) {
+    super(name, "pollen");
+  }
+   eat(food) {
+    if (food == this.favoriteFood) {
+      super.eat(food);
+      this.sleep();
+    } else {
+      console.log("YUCK!!! " + this.name + " will not eat " + food)
+    }
+  }
+  sleep() {
+    console.log(this.name + " never sleeps");
+  }
+}
+class ZooKeeper{
+  constructor(name) {
+    this.name = name;
+  }
+  feedAnimals(animals,food){
+    console.log( this.name+" is feeding "+ food+ " to "+ animals.length +" of " + Animal.getPopulation()+ " total animals.")
+    for(let i=0;i<animals.length;i++){
+      animals[i].eat(food);
+  }
+  }
+}
 function run() {
-    sleep("Tigger");
-  }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
-  }
+  var tigger = new Tiger("Tigger");
+  tigger.eat("meat");
+  var pooh = new Bear("Pooh");
+  pooh.eat("fish");
+  pooh.eat("meat");
+  var rarity = new Unicorn("Rarity");
+  rarity.eat("marshmallows");
+  rarity.sleep();
+  var gemma = new Giraffe("Gemma");
+  gemma.eat("meat");
+  gemma.eat("leaves");
+  gemma.sleep();
+  var stinger = new Bee("Stinger");
+  stinger.eat("ice cream");
+  stinger.eat("pollen");
+  var zooKeeper = new ZooKeeper("Zoebot");
+  arr=[new Tiger("tigger"), new Bear("pooh"), new Unicorn("rarity"), new Giraffe("gemma"), new Bee("stinger")]
+ zooKeeper.feedAnimals(arr,"pizza");
+ console.log(Animal.getPopulation());
+}
+run();
